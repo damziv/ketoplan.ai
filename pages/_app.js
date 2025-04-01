62a824d33626542bcf17c8e2b0f9eb7eb863e797
Damir Zivkusic <damirzivkusic@Damirs-MacBook-Pro.local>
2025-04-01 20:05:47 +0200

    favicon added to _app-js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+import Head from 'next/head';
 
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.png" />
+        <meta name="theme-color" content="#ffffff" />
+        <meta name="description" content="Your personalized Keto Meal Plan App" />
+        <meta property="og:title" content="AI Keto Meal Plan" />
+        <meta property="og:image" content="/favicon.png" />
+        {/* You can also try: <link rel="shortcut icon" href="/favicon.png" /> */}
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
 export default MyApp;
