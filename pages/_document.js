c4fe9c2d5e1b9fcbb4b76387e269e2c76ff6f517
Damir Zivkusic <damirzivkusic@Damirs-MacBook-Pro.local>
2025-04-11 13:21:33 +0200

    facebook pixel i frontend promjene

--- pages/_document.js
+++ pages/_document.js
@@ -13,6 +13,36 @@ export default function Document() {
         <meta property="og:image" content="/favicon.png" />
         <meta property="og:description" content="Get your personalized 5-day keto meal plan with full recipes and PDF download." />
       </Head>
+{/* ✅ Meta Pixel Script */}
+<Script
+        id="fb-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1185363959699161');
+            fbq('track', 'PageView');
+          `,
+        }}
+      />
+      
+      {/* Optional noscript fallback */}
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: 'none' }}
+          src="https://www.facebook.com/tr?id=1185363959699161&ev=PageView&noscript=1"
+        />
+      </noscript>
+
       <body>
         <Main />
         <NextScript />
