f8f2ab46c695fa023ed7db2f4853b2ecc836feec
Damir Zivkusic <damirzivkusic@Damirs-MacBook-Pro.local>
2025-07-10 13:11:39 +0200

    removed excess pixel

--- pages/_document.js
+++ pages/_document.js
@@ -42,38 +42,7 @@ export default function Document() {
           height="1"
           width="1"
           style={{ display: 'none' }}
-          src="https://www.facebook.com/tr?id=1185363959699161&ev=PageView&noscript=1"
-        />
-      </noscript>
-
-
-      {/* ✅ VericonEMUS Meta Pixel Script */}
-      <Script
-        id="fb-pixel"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-          !function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '9290041274439815');
-          fbq('track', 'PageView');
-          `,
-        }}
-      />
-      
-      {/* Optional noscript fallback */}
-      <noscript>
-        <img
-          height="1"
-          width="1"
-          style={{ display: 'none' }}
-          src="https://www.facebook.com/tr?id=9290041274439815&ev=PageView&noscript=1"
+          src="https://www.facebook.com/tr?id=1142296627565768&ev=PageView&noscript=1"
         />
       </noscript>
 
