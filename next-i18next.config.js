2b5aeb0bcb19fe9fd940cd0894eff671499727a2
Damir Zivkusic <damirzivkusic@Damirs-MacBook-Pro.fritz.box>
2025-07-28 13:25:38 +0200

    polish locales added, only common

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     i18n: {
       defaultLocale: 'en',
-      locales: ['en', 'hr', 'de',],
+      locales: ['en', 'hr', 'de', 'pl',],
       domains: [
         {
           domain: 'keto-meal.com',
