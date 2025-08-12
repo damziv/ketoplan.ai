575d7b19c72ff624874bf72b1297c719533d3240
Damir Zivkusic <damirzivkusic@Damirs-MacBook-Pro.local>
2025-08-12 10:12:12 +0200

    next-18next.config added naturalfix domain

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -19,6 +19,19 @@ module.exports = {
           domain: 'pl.keto-meal.com',
           defaultLocale: 'pl',
         },
+              // Optional: subdomains for naturalfix.me in other languages
+      {
+        domain: 'hr.naturalfix.me',
+        defaultLocale: 'hr',
+      },
+      {
+        domain: 'de.naturalfix.me',
+        defaultLocale: 'de',
+      },
+      {
+        domain: 'pl.naturalfix.me',
+        defaultLocale: 'pl',
+      },
       ],
       localeDetection: false // ✅ Either `true` or `false`, no quotes!
     },
